df8ee1a3609df282a161be69193f8afc62a51399
Mariano Prazmowski <mnprazmowski@gmail.com>
2023-04-01 17:05:43 -0300

    invertir y ver inversion

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const username = "elon";
 const password = "musk";
 let saldo = 10000;
+let montoInversion = 0;
 
 function validarUsuario(inputUsername, inputPassword) {
   if (inputUsername === username && inputPassword === password) {
@@ -10,7 +11,7 @@ function validarUsuario(inputUsername, inputPassword) {
   }
 }
 
-alert("Bienvenido, por favor ingrese sus credenciales.");
+alert("Bienvenido al sistema, por favor ingrese sus credenciales.");
 let ingresoExitoso = false;
 while (!ingresoExitoso) {
   const inputUsername = prompt("Ingresa tu usuario:");
@@ -19,8 +20,9 @@ while (!ingresoExitoso) {
   if (validarUsuario(inputUsername, inputPassword)) {
     let opcion;
     do {
-      opcion = prompt(`Inicio de sesión exitoso! Tu saldo disponible es de $${saldo}. ¿Qué deseas hacer?\n\n1. Retirar saldo\n2. Ingresar dinero a su cuenta\n3. Desloguearse`);
+      opcion = prompt(`Inicio de sesión exitoso! Tu saldo disponible es de $${saldo}. ¿Qué deseas hacer?\n\n1. Retirar saldo\n2. Ingresar dinero a la cuenta\n3. Invertir en plazo fijo\n4. Ver inversion activa\n5. Desloguearse`);
       switch (opcion) {
+
         case "1":
           const monto = parseInt(prompt("Ingresa el monto a retirar:"));
           if (isNaN(monto)) {
@@ -32,6 +34,7 @@ while (!ingresoExitoso) {
             alert(`Retiro exitoso! Tu nuevo saldo disponible es de $${saldo}.`);
           }
           break;
+
         case "2":
           const montoIngreso = parseInt(prompt("Ingresa el monto a ingresar:"));
           if (isNaN(montoIngreso)) {
@@ -41,7 +44,42 @@ while (!ingresoExitoso) {
             alert(`Ingreso exitoso! Tu nuevo saldo disponible es de $${saldo}.`);
           }
           break;
+
         case "3":
+          let montoInvertir = parseInt(prompt(`Ingresa el monto a invertir (saldo actual: $${saldo}):`));
+          if (isNaN(montoInvertir)) {
+            alert("El monto ingresado es inválido.");
+          } else if (montoInvertir > saldo) {
+            alert("El monto ingresado es mayor que el saldo disponible.");
+          } else {
+            const tasaInteresAnual = 0.75;
+            const tasaInteresMensual = tasaInteresAnual / 12;
+            const interesGenerado = montoInvertir * tasaInteresMensual;
+            const montoTotal = montoInvertir + interesGenerado;
+            const confirmacionInversion = confirm(`Usted desea invertir $${montoInvertir} a un plazo fijo de 30 días con una tasa de interés anual del 75%. El interés generado en 30 días será de $${interesGenerado.toFixed(2)}. El monto total al final del plazo será de $${montoTotal.toFixed(2)}. ¿Desea confirmar la inversión?`);
+            if (confirmacionInversion) {
+              saldo -= montoInvertir;
+              alert(`Inversión realizada con éxito. Su nuevo saldo disponible es de $${saldo}.`);
+              montoInversion = montoInvertir;
+            } else {
+              alert("Inversión cancelada.");
+            }
+          }
+          break;
+
+        case "4":
+          if (montoInversion === 0) {
+            alert("No tienes una inversión activa.");
+          } else {
+            const tasaInteresAnual = 0.75;
+            const tasaInteresMensual = tasaInteresAnual / 12;
+            const interesGenerado = montoInversion * tasaInteresMensual;
+            const montoTotal = montoInversion + interesGenerado;
+            alert(`Tu inversión activa es de $${montoInversion}, vence en 30 días y te dara un monto total de $${montoTotal.toFixed(2)}.`);
+          }
+          break;
+
+        case "5":
           alert("Gracias por usar nuestros servicios.");
           ingresoExitoso = true;
           break;
@@ -49,7 +87,7 @@ while (!ingresoExitoso) {
           alert("Opción inválida. Por favor, elige una opción válida.");
           break;
       }
-    } while (opcion !== "3" && !ingresoExitoso);
+    } while (opcion !== "5" && !ingresoExitoso);
   } else {
     alert("Usuario o contraseña incorrectos. Inténtalo de nuevo.");
   }
